3f9eb23c64c5cc71ef1d98ab96227f848ca69bb7
ewall-divi <ericw@diviup.org>
2020-09-17 11:22:58 -0500

    Gradle Stuff
    Gradle Stuff

---
+++ sampleapp/src/main/kotlin/com/kofanity/sampleapp/App.kt
@@ -0,0 +1,13 @@
+package com.kofanity.sampleapp
+
+import com.kofanity.filter.Kofanity
+
+class App {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Welcome to Kofanity")
+            
+        }
+    }
+}
